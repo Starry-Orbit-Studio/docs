bd76ec98a10dffdbea575aa7768c4da7ddcddfa3
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-07-31 12:25:41 +0800

    :ambulance: 修复路径
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- vuepress.theme.ts
+++ vuepress.theme.ts
@@ -33,7 +33,7 @@ export default hopeTheme({
       // sidebar
       sidebar: sidebar.zh,
 
-      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
+      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
 
       displayFooter: true,
 
@@ -42,7 +42,7 @@ export default hopeTheme({
         editLink: "在 GitLab 上编辑此页",
       },
     },
-    
+
     "/en/": {
       // navbar
       navbar: navbar.en,
@@ -55,7 +55,7 @@ export default hopeTheme({
       // sidebar
       sidebar: sidebar.en,
 
-      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.',
+      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.',
 
       displayFooter: true,
 
