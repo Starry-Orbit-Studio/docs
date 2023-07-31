a690325a8697383713cdffe3856d47c8db669077
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-07-31 12:25:20 +0800

    :tada: 初始化提交
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ vuepress.config.ts
@@ -0,0 +1,28 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./vuepress.theme";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "星辰之光文档站",
+      description: "星辰之光文档站",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Extreme Starry Docs",
+      description: "Extreme Starry Docs",
+    },
+  },
+
+  theme,
+
+  temp: '.temp',
+  cache: '.cache',
+  public: 'public',
+  dest: 'dist',
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ vuepress.theme.ts
@@ -0,0 +1,167 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import * as navbar from "./navbar";
+import * as sidebar from "./sidebar";
+
+export default hopeTheme({
+  hostname: "https://demo.github.io",
+
+  iconAssets: "fontawesome-with-brands",
+
+  logo: "/logo.png",
+
+  repo: "https://gitlab.com/starry-orbit-studio/extreme-starry/edn",
+
+  docsRepo: "https://gitlab.com/starry-orbit-studio/extreme-starry/edn",
+
+  docsBranch: "master",
+
+  docsDir: "docs",
+
+  locales: {
+    /**
+     * Chinese locale config
+     */
+    "/": {
+      // navbar
+      navbar: navbar.zh,
+
+      author: {
+        name: "星轨工作室",
+        url: "https://gitlab.com/starry-orbit-studio",
+      },
+
+      // sidebar
+      sidebar: sidebar.zh,
+
+      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
+
+      displayFooter: true,
+
+      // page meta
+      metaLocales: {
+        editLink: "在 GitLab 上编辑此页",
+      },
+    },
+    
+    "/en/": {
+      // navbar
+      navbar: navbar.en,
+
+      author: {
+        name: "Starry Orbit Studio",
+        url: "https://gitlab.com/starry-orbit-studio",
+      },
+
+      // sidebar
+      sidebar: sidebar.en,
+
+      footer: '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.',
+
+      displayFooter: true,
+
+      metaLocales: {
+        editLink: "Edit this page on GitLab",
+      },
+    },
+  },
+
+  plugins: {
+    // All features are enabled for demo, only preserve features you need here
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      demo: true,
+      echarts: true,
+      figure: true,
+      flowchart: true,
+      gfm: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      katex: true,
+      mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      presentation: ["highlight", "math", "search", "notes", "zoom"],
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+      vuePlayground: true,
+    },
+
+    // uncomment these if you want a pwa
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
